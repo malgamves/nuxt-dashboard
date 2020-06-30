e5459d994415d8ba5aa37459e27d845cad37089b
Daniel Phiri <malgamves@gmail.com>
2020-06-30 23:43:19 +0200

    clean up

--- pages/index.vue
+++ pages/index.vue
@@ -62,10 +62,8 @@ import { QueryRenderer } from "@cubejs-client/vue";
 
 // Importing our application components
 import BaseContainer from "~/components/containers/Base.vue";
-import Logo from "~/components/Logo.vue";
 import Bar from "~/components/BarChart.vue";
 import Pie from "~/components/PieChart.vue";
-import Area from "~/components/AreaChart.vue";
 import CityCard from "~/components/CityCard.vue";
 import CountryCard from "~/components/CountryCard.vue";
 import LanguageCard from "~/components/LanguageCard.vue";
@@ -73,16 +71,14 @@ import LanguageCard from "~/components/LanguageCard.vue";
 
 // Our Cube.js Key and API URL
 const cubejsApi = cubejs(
-  "9d0a27748a060749b6e6aaae4e9b946cd9007a065f0ce71267596dfd2e9938305d3d7d5909bcbf9444d9f1f78d9c27dbcba7592b85cce76381350170ae9e5aaf",
+  "<API KEY>",
   { apiUrl: "http://localhost:4000/cubejs-api/v1" }
 );
 
 export default {
   components: {
-    Logo,
     Bar,
     Pie,
-    Area,
     QueryRenderer,
     BaseContainer,
     CityCard,
